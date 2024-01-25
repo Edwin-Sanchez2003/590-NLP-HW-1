90e3d72588bf2c0d64370d959b163505cf454f2d
Edwin Sanchez <edwsanch@iu.edu>
2024-01-24 23:53:04 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+590 Natural Language Processing | Edwin Sanchez
+# Homework 1 - Problem 5
+The code in this repo is used to complete Problem 5 of Homework 1 for 590 Natural Language Processing.
+
+## Environment Setup
+
+* **Python Version:** 3.11.5 *(should work with python versions 3.11.X. Type Hints may lead to compatibility issues with older python versions)*
+* **OS Used:** Windows Subsystem for Linux, Version 2 (WSL2) on Windows 11
+
+1. Create a python environment: `conda create -n hw-1 python=3.11.5`
+2. Activate the environment: `conda activate hw-1`
+3. Install the required packages: `pip install -r requirements.txt`
+
+## data_generator.py
+
+## analysis.py
+
+
+
+## Useful Commands
+* **Extract messages from Original Data:** `python data_generator.py -i ./original_data/test.jsonl -o ./data.txt`
---
+++ data_generator.py
@@ -0,0 +1,83 @@
+"""
+    Homework 01
+    590 Natural Language Processing
+    Edwin Sanchez
+    
+    Dataset Generator
+    Reads the data from a json file,
+    extracting only the messages, and writes
+    them onto lines in a text file.
+"""
+
+# Packages
+import json # read json files
+import argparse
+
+# description for argparse
+descr = """
+Reads the data from a json file,
+extracting only the messages, and writes
+them onto lines in a text file.
+"""
+
+# argument parser object
+parser = argparse.ArgumentParser(
+                    prog='Dataset Generator',
+                    description=descr)
+
+# path to file to be extracted
+parser.add_argument('-i', '--input_file_path', help="The path to the file you want to extract data from. Must be in JSON file format.")
+parser.add_argument('-o', '--output_file_path', help="The path to the file you want to write the extracted data to. Assumes txt file.")
+
+# parse args
+args = parser.parse_args()
+INPUT_FILE_PATH = str(args.input_file_path)
+OUTPUT_FILE_PATH = str(args.output_file_path)
+
+
+def main():
+    # load the json file
+    data = load_json_file(path=INPUT_FILE_PATH)
+    
+    # extract message data
+    message_data = extract_data(data=data)
+    
+    # write back to a text file
+    write_to_txt(path=OUTPUT_FILE_PATH, lines=message_data)
+# end main
+
+
+# extracts message data from a json file
+# returns a list of strings, each string 
+# being a message.
+def extract_data(data:list[dict])->list[str]:
+    messages:list[str] = []
+    for sample in data:
+        for msg in sample["messages"]:
+            messages.append(msg)
+    return messages
+# end extract_data
+
+
+# write a list of strings to a text file
+def write_to_txt(path:str, lines:list[str])->None:
+    with open(file=path, mode='w') as file:
+        for line in lines:
+            file.write(line + '\n')
+# end write_to_txt
+
+
+# takes a path to a json file and loads it into a python dictionary
+def load_json_file(path:str)->list[dict]:
+    lines:list[dict] = []
+    with open(file=path, mode="r") as file:
+        samples = list(file)
+        for line in samples:
+            lines.append(json.loads(line))
+    return lines
+# end load_json_file
+
+
+if __name__ == "__main__":
+    main()
+    
